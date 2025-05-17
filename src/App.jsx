f38989ff99cf2b3d97ea7de3fa5b04c0deb1637d
aziz <azizcode92@gmail.com>
2025-05-17 15:41:15 +0200

    Props lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { LessonComponent } from "./components/lessonComponent.jsx";
 import { LessonJSX } from "./components/lessonJSX.jsx";
-import { LessonProps } from "./components/lessonProps.jsx";
+import { LessonProps, LessonProps2 } from "./components/lessonProps.jsx";
+
 function App() {
-  let product_Id = 1001
+  const product_Id = 1001;
 
   return (
     <>
       <LessonComponent />
       <LessonJSX />
-      <LessonProps name="Samsuag" product_Id={product_Id} />
+
+      <LessonProps name=": Samsung" product_Id={product_Id} />
+      <LessonProps2 name=": Dell" product_Id={63} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/lessonProps.jsx
+++ src/components/lessonProps.jsx
@@ -1,10 +1,21 @@
-// export const LessonProps = (props) =>{
 export const LessonProps = ({product_Id, name}) =>{
     return (
         <>
-        <h1>Love form porps</h1>
-        {/* <h1>{props.product_Id} {props.name}</h1> */}
+        <h1>Informationen zum Produkt</h1>
+
+       
         <h1>{product_Id} {name}</h1>
+        </>
+    )
+}
+
+export const LessonProps2 = (props) =>{
+    return (
+        <>
+        <h1>Produktdetails</h1>
+
+        <h1>{props.product_Id} {props.name}</h1>
+
         </>
     )
 }
